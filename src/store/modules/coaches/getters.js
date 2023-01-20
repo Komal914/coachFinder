d9a886001fdbcef9d12ba65e95f7650eca26e24a
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-19 23:00:38 -0500

    connected coachlist to data in vuex

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  coaches(state) {
+    return state.coaches;
+  },
+  hasCoaches(state) {
+    //checks if we have any coaches, returns true or false
+    return state.coaches && state.coaches.length > 0;
+  },
+};
