fe790f0d41f117a24e1f10b28233e149740a0bc4
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-22 17:48:58 -0500

    new request is sent to store for specific coach

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  contactCoach(context, payload) {
+    //object to send to store
+    const newRequest = {
+      id: new Date().toISOString(),
+      coachId: payload.coachId,
+      userEmail: payload.email,
+      message: payload.message,
+    };
+    //pass data to mutation
+    context.commit('addRequest', newRequest);
+  },
+};
--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,1 +1,6 @@
-export default {};
+export default {
+  addRequest(state, payload) {
+    //dds to state requests array
+    state.requests.push(payload);
+  },
+};
