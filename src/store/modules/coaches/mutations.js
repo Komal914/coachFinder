b86a475fd1d7b960490585f8fa783337ec1b053c
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-25 17:41:28 -0500

    fixed id bug

--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -2,4 +2,8 @@ export default {
   registerCoach(state, payload) {
     state.coaches.push(payload); //add new coach
   },
+  //passing over coaches from firebase
+  setCoaches(state, payload) {
+    state.coaches = payload;
+  },
 };
