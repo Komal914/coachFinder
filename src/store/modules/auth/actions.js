f5b208214978c26a62b3203352d651cdda257bfb
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-28 03:04:08 -0500

    local storage to avoid restarts of app

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -64,6 +64,8 @@ export default {
   },
 
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
     context.commit('setUser', {
       token: null,
       userId: null,
