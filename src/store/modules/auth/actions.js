0da3b2255cfbd013764f938a2ef60585c7e31967
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-28 02:42:15 -0500

    combined login + signup actions into auth

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,60 +1,43 @@
 export default {
   async login(context, payload) {
-    //http request
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBo1sQShnIBEwoeccS1We2P05fnGf8p7pA',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-    if (!response.ok) {
-      console.log('ResponseData bad: ', responseData);
-      const error = new Error(
-        response.message ||
-          'Failed to authenticate signup. Check your login data.'
-      );
-      throw error;
-    }
-    console.log('ResponseData: ', responseData);
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresin,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
-
   async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+
+  async auth(context, payload) {
+    //mode = signup or login
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBo1sQShnIBEwoeccS1We2P05fnGf8p7pA';
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBo1sQShnIBEwoeccS1We2P05fnGf8p7pA';
+    }
     //http request
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBo1sQShnIBEwoeccS1We2P05fnGf8p7pA',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
     if (!response.ok) {
-      console.log('ResponseData bad: ', responseData);
       const error = new Error(
-        response.message ||
-          'Failed to authenticate signup. Check your login data.'
+        response.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
-    console.log('ResponseData: ', responseData);
 
     context.commit('setUser', {
       token: responseData.idToken,
@@ -62,6 +45,7 @@ export default {
       tokenExpiration: responseData.expiresin,
     });
   },
+
   logout(context) {
     context.commit('setUser', {
       token: null,
